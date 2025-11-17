38ff00899fb203b5f31e810100feab8b8c6e73fd
maubri96 <rauzav96@outlook.es>
2025-11-17 14:52:57 -0600

    Correcion en configuracion

--- src/config/db.js
+++ src/config/db.js
@@ -1,29 +1,27 @@
-import express from "express";
-import cors from "cors";
-import vehiculoRoute from "./routes/vehiculoRoute.js";
-import registroRoute from "./routes/registroRoute.js";
-import { pool } from "./db.js";
+import pg from "pg";
+import dotenv from "dotenv";
 
-const app = express();
-const port = process.env.PORT || 5000;
+dotenv.config();
 
-app.use(cors());
-app.use(express.json());
+const db = new pg.Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
 
-app.use("/api", vehiculoRoute);
-app.use("/api", registroRoute);
-
-// Conexión a la base de datos antes de levantar el servidor
-pool.connect()
-  .then(() => {
-    console.log("✅ Base de datos conectada");
-
-    app.listen(port, () => {
-      console.log(`Servidor corriendo en puerto ${port}`);
-    });
-
-  })
+// Test de conexión
+db.connect()
+  .then(() => console.log("✅ Base de datos conectada"))
   .catch((err) => {
     console.error("❌ Error al conectar la base de datos:", err);
     process.exit(1);
   });
+
+db.on("error", (err) => {
+  console.error("El pool de PostgreSQL está inactivo", err);
+  process.exit(-1);
+});
+
+export const query = (text, params) => db.query(text, params);
