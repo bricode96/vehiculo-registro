f83d3fe20055254880dab09187fb147687a1eaa6
maubri96 <rauzav96@outlook.es>
2025-11-19 09:14:21 -0600

    Configuracion post de registroService3

--- src/service/registroService.js
+++ src/service/registroService.js
@@ -3,10 +3,10 @@ import { query } from "../config/db.js"
 export const getRegistro = async () => {
     const { rows } = await query('SELECT * FROM vehicle_logs');
     return rows;
-} 
+}
 
 export const postRegistro = async (registroData) => {
-    const { vehiculo_id, motorista_nombre, km_salida, fecha_hora_salida, destino_notas, status } = registroData;
+    const {vehiculo_id, motorista_nombre, km_entrada, fecha_hora_entrada, km_salida, fecha_hora_salida, destino_notas, status } = registroData;
 
     if (!vehiculo_id) throw new Error("vehiculo_id es obligatorio");
     if (!motorista_nombre) throw new Error("motorista_nombre es obligatorio");
@@ -23,11 +23,12 @@ export const postRegistro = async (registroData) => {
 
     const insert = await query(
         `INSERT INTO vehicle_logs 
-         (vehiculo_id, motorista_nombre, km_salida, fecha_hora_salida, destino_notas, status)
-         VALUES ($1, $2, $3, $4, $5, $6)
-         RETURNING *`,
-        [vehiculo_id, motorista_nombre, km_salida, fecha_hora_salida, destino_notas, status]
+     (vehiculo_id, motorista_nombre, km_entrada, fecha_hora_entrada, km_salida, fecha_hora_salida, destino_notas, status)
+     VALUES ($1,$2,$3,$4,$5,$6,$7,$8)
+     RETURNING *`,
+        [vehiculo_id, motorista_nombre, km_entrada, fecha_hora_entrada, km_salida, fecha_hora_salida, destino_notas, status]
     );
+
 
     return insert.rows[0];
 };
@@ -36,9 +37,9 @@ export const postRegistro = async (registroData) => {
 export const putRegistro = async (registroData, registroId) => {
     let {
         vehiculo_id, motorista_nombre, km_entrada, fecha_hora_entrada, status, km_salida = null, fecha_hora_salida = null,
-        destino_notas = null} = registroData;
+        destino_notas = null } = registroData;
 
-    // Verificar que el vehículo exista y esté activo
+    // Verificar que el vehículo exista y esté activoz
     const { rows: vehiculoRows } = await query(
         "SELECT id, status FROM vehiculos_td WHERE id = $1",
         [vehiculo_id]
@@ -59,15 +60,15 @@ export const putRegistro = async (registroData, registroId) => {
         `UPDATE vehicle_logs SET vehiculo_id = $1, motorista_nombre = $2, km_entrada = $3, fecha_hora_entrada = $4,
              status = $5, km_salida = $6, fecha_hora_salida = $7, destino_notas = $8, updated_at = CURRENT_TIMESTAMP
          WHERE log_id = $9 RETURNING *`,
-        [vehiculo_id, motorista_nombre, km_entrada, fecha_hora_entrada, status, km_salida, 
-        fecha_hora_salida, destino_notas, registroId
+        [vehiculo_id, motorista_nombre, km_entrada, fecha_hora_entrada, status, km_salida,
+            fecha_hora_salida, destino_notas, registroId
         ]
     );
 
     return rows[0];
 };
 
-  export const deleteRegistro = async (registroId) =>{
+export const deleteRegistro = async (registroId) => {
     const { rowCount } = await query(`DELETE FROM vehicle_logs WHERE log_id = $1`,
         [registroId]
     )
